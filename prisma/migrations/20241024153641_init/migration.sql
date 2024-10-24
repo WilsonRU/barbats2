6a59c5c97e51694d719c1bd894cbcb3c3a8bcc38
Wilson Moura <wilo.moura3@gmail.com>
2024-10-24 12:37:48 -0300

    Migração da tabela usuario com deletedAt

--- prisma/migrations/20241024153641_init/migration.sql
+++ prisma/migrations/20241024153641_init/migration.sql
@@ -1,13 +1,13 @@
-PRAGMA defer_foreign_keys=ON;
-PRAGMA foreign_keys=OFF;
+-- CreateTable
 CREATE TABLE "User" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
+    "updatedAt" DATETIME NOT NULL,
+    "deletedAt" DATETIME
 );
+
+-- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
-PRAGMA foreign_keys=ON;
-PRAGMA defer_foreign_keys=OFF;
