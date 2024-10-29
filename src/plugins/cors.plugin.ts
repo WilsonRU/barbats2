803d06f4fdb9138178fabfb68d1caca17d3a4f85
Wilson Moura <wilo.moura3@gmail.com>
2024-10-29 19:48:36 -0300

    Configurado o plugin de CORS

---
+++ src/plugins/cors.plugin.ts
@@ -0,0 +1,25 @@
+import type { FastifyInstance, FastifyPluginAsync } from "fastify";
+import cors, { FastifyCorsOptions } from "@fastify/cors";
+
+const corsPlugin: FastifyPluginAsync = async (app: FastifyInstance) => {
+	const corsOptions: FastifyCorsOptions = {
+		origin: (origin, cb) => {
+            const hostname = new URL(origin as string).hostname
+			const allowedOrigins = [
+				"localhost",
+			];
+
+			if (!origin || allowedOrigins.includes(hostname)) {
+				cb(null, true);
+				return;
+			}
+			cb(new Error("Destino não autorizado"), false);
+		},
+		methods: ["GET", "POST", "PUT", "DELETE"],
+		credentials: true,
+	};
+	app.register(cors, corsOptions);
+};
+
+export default corsPlugin;
+
