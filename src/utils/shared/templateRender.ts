91c86d337d0d161fa8d9d5d3b2724428082db61e
Wilson Moura <wilo.moura3@gmail.com>
2025-02-03 21:14:45 -0300

    Adiciona o EJS como template engine para envios de email com HTML

---
+++ src/utils/shared/templateRender.ts
@@ -0,0 +1,19 @@
+import ejs from "ejs";
+import { join } from "path";
+import { readFileSync } from "fs";
+
+class TemplateRenderer {
+	private templatesDir: string;
+
+	constructor() {
+		this.templatesDir = join(__dirname, "../../templates");
+	}
+
+	render(file: string, data: object = {}): string {
+		const templatePath = join(this.templatesDir, file);
+		const templateContent = readFileSync(templatePath, "utf-8");
+		return ejs.render(templateContent, data);
+	}
+}
+
+export default new TemplateRenderer();
